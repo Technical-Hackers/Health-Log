4f738e426dbd3dba4dd26a40bec093c15ced8358
DANISH <danishjamal.104@gmail.com>
2020-04-11 13:52:34 +0530

    Firebase synced

---
+++ app/src/main/java/com/example/healthlog/model/Hospital.java
@@ -0,0 +1,12 @@
+package com.example.healthlog.model;
+
+public class Hospital {
+
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String district;
+
+
+}
