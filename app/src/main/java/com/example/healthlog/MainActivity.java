77d2b5a0b96a2fd439762e8bfdc32f4317391f12
Bbiswabasu <58428413+Bbiswabasu@users.noreply.github.com>
2020-12-12 11:34:44 +0530

    Menu items shifted

--- app/src/main/java/com/example/healthlog/MainActivity.java
+++ app/src/main/java/com/example/healthlog/MainActivity.java
@@ -65,9 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_logout, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
